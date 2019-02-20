1925709b42fb9d4d6d63a6010e0738a9971cabed
student12 <student12@student12s-MacBook-Air.local>
2019-02-20 01:33:08 -0600

    Forgotten changes

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    enum lightModes {
+        case off
+        case on
+    }
+    
+    var currenMode: lightModes = .off {
+        didSet {
+            switch currenMode {
+            case .on:
+                nextMode = .off
+                print("on")
+                view.backgroundColor = .white
+            case .off:
+                nextMode = .on
+                print("off")
+                view.backgroundColor = .black
+            }
+        }
+    }
+    
+    var nextMode: lightModes = .on
+    var lightOn  = true
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        currenMode = nextMode
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currenMode = .off
     }
 
 
